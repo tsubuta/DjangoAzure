5766fdb58f98ebdd145c9aa5d954b88b96a0ad30
Anas Mastour <mastour.anas@gmail.com>
2017-06-14 22:31:23 +0000

    create class model and add foler

--- app/models.py
+++ app/models.py
@@ -5,3 +5,15 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+
+class test(models.Model):
+    """
+    Description: Model Description
+    """
+    nom = models.CharField(max_length=250)
+    prenom = models.CharField(max_length=250)
+    picture = models.FileField(upload_to="picture")
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        pass
