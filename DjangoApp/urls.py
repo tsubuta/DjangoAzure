761a0b0a8b36628caa66679d4a97d2f39e22f510
Anas Mastour <mastour.anas@gmail.com>
2017-06-15 00:22:16 +0000

    u

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -42,4 +42,3 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
